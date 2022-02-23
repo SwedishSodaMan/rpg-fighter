8781d207416f94cc63505eb1ece4e9475ddde276
SwedishSodaMan <joaquin1.torres@hotmail.com>
2022-02-23 15:10:50 +0100

    Work in progress

--- resources.py/main.py
+++ resources.py/main.py
@@ -1,12 +1,11 @@
-from queue import Empty
 import resources
-from resources import Character, Goblin
-from random import choice, randint, shuffle
+from resources import Character, Goblin, save_character, load_characters, create_characters
+from random import randint, shuffle, choice
 
 def fight(players : list, enemies : list):
-    participants = players + enemies # skapa initiativordning
+    participants = players + enemies # skapa initiativordning 
     shuffle(participants)
-
+    
     for char in participants:
         target = ""
         # är karaktären en spelare eller en goblin?
@@ -14,40 +13,54 @@ def fight(players : list, enemies : list):
             target = choice(enemies)
         else:
             target = choice(players)
-
+            
         target.take_damage(char.get_attack())
         
         if target.get_health() == 0:
-            print(f"{target.get_name()} has died! ")
+            print(f"{target.get_name()} has died!")
             if type(target) == Goblin:
                 enemies.remove(target)
             else:
                 players.remove(target)
             participants.remove(target)
         else:
-            print(f"{target.get_name()} has {target.get_health()} hp remaining. ")
-
+            print(f"{target.get_name()} has {target.get_health()} hp remaining.")
+        
         if len(enemies) == 0 or len(players) == 0:
             break
+            
+    
 if __name__ == "__main__":
     enemies = []
-    players = []
-    emy = Character("Emy", 20, 5, 3)
-    nick = Character("Nick", 15, 2, 1)
-    players.append(emy)
-    players.append(nick)
+    players = load_characters()
 
-    enemies.append(Goblin(10, 3, 2, 1))
-    enemies.append(Goblin(15, 2, 1, 2))
-    enemies.append(Goblin(12, 3, 1, 3))
+    print("Do you want to create new characters? ")
+    create_new = input("y/n): ")
+    if(create_new.lower() == "y"):
+        how_many = int(input("how many characters do you want to create? "))
+        for i in range(how_many):
+            players.append(create_characters())
+
+    amount_of_goblins = int(input("How many goblins do you want to fight?: "))
+    for i in range(amount_of_goblins):
+        enemies.append(Goblin(randint(10, 15), randint(0, 2), 1+1))
+    
 
     round = 1
     while len(enemies) != 0 and len(players) != 0:
-        print(f"ROUND {round}, FIGHT! ")
+        print(f"ROUND {round}, FIGHT!")
         fight(players, enemies)
         print()
         round +=1
-    if len(enemies) == 0:
-        print("The players won! ")
+    
+    if len(enemies) == 0:   
+        print("The players won!")
+        print("Would you like to save the remaining characters?")
+        save_progress = input("(y/n): ")
+        if save_progress == "y":
+            save_character(players)
+        else:
+            print("No progress was saved.")
+        
     elif len(players) == 0:
-        print("The Goblins won! ")
+        print("The Goblins won!")
--- resources.py/resources.py
+++ resources.py/resources.py
@@ -1,14 +1,51 @@
+from asyncore import write
+from email.base64mime import header_length
+import encodings
+from lib2to3.pgen2.token import NAME
+from os import name
+from random import randint
+from urllib.request import HTTPDefaultErrorHandler
+from xml.dom.pulldom import CHARACTERS
+from random import randint, choice
+
+# classes
+class Weapon:
+    def __init__(self, name, damage) -> None:
+        self.name = name
+        self.damage = damage
+
+    def get_damage(self):
+        return self.damage
+
+# BRYTER MOT STANDARD
+
+GOBLIN_WEAPONS = [Weapon("Rusty Cleaver", 2),
+                  Weapon("Rusty Spear", 3),
+                  Weapon("Stone Axe", 1)]
 class Character:
 
-    def __init__(self, name, health, attack, armor):
+    def __init__(self, name, health, armor):
         self.name = name
         self.health = health
-        self.attack = attack
         self.armor = armor
+        self.generate_weapon()
+        self.attack = self.weapon.get_damage()
     
     def __str__(self) -> str:
         return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
     
+    def generate_weapon(self):
+        random_weapon = randint(0, 100)
+        if random_weapon < 20: self.weapon= Weapon("Shortsword", 3)
+        elif random_weapon >= 20 and random_weapon < 40: self.weapon = Weapon("Broadsword", 5)
+        elif random_weapon >= 40 and random_weapon < 60: self.weapon = Weapon("Small knife", 1)
+        elif random_weapon >= 60 and random_weapon < 80: self.weapon = Weapon("Spear", 2)
+        elif random_weapon >= 80 and random_weapon < 90: self.weapon = Weapon("Halberd", 4)
+        elif random_weapon >= 90 and random_weapon < 95: self.weapon = Weapon("Morning Star", 4)
+
+
+
+
     def get_attack(self): # tidigare damage
         return self.attack
 
@@ -23,16 +60,20 @@ class Character:
 
     def get_name(self):
         return self.name
+    
+    def get_attributes(self):
+        return self.name, self.health, self.attack, self.armor
 
 class Goblin:
 
 
-    def __init__(self, health, attack, armor, id):
+    def __init__(self, health, armor, id):
         
         self.health = health
-        self.attack = attack
         self.armor = armor
         self.id = id
+        self.weapon = choice(GOBLIN_WEAPONS)
+        self.attack = self.weapon.get_damage()
     
     def __str__(self) -> str:
         return f"Goblin #{self.id}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
@@ -51,3 +92,41 @@ class Goblin:
 
     def get_name(self):
         return f"Goblin #{self.id}"
+
+def save_character(chars : list()):
+    save_list = []  
+    for character in chars:   
+        name, health, attack, armor = character.get_attributes()
+        save_string = f"{name}/{health}/{attack}/{armor}\n"
+        save_list.append(save_string)
+
+    with open("saved_characters.txt", "w", encoding="utf8") as f:
+        for line in save_list:
+            f.write(line)
+        print(f"Character has been succesfully saved! ")
+
+def load_characters():
+    characters = []
+    with open("saved_characters.txt", "r", encoding="utf8") as f:
+        for line in f.readlines():
+            attributes = line.split("/")
+            char = Character(attributes[0],
+                             int(attributes[1]),
+                             int(attributes[2]),
+                             int(attributes[3]))
+            characters.append(char)
+    return characters
+
+def create_characters():
+    print("Welcome to the character creator! ")
+    name = input("What is your character called?: ")
+    health = randint(10, 30)
+    attack = randint(1, 5)
+    armor = randint(0, 5)
+
+    return_char = Character(name, health, attack, armor)
+    
+    print()
+    print(return_char)
+    print("Character has been created")
+    return return_char
